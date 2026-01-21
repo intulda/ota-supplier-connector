1c186b5705fc3f53538a054c06de2cd25a55e22d
김보근 <intulda@naver.com>
2026-01-21 10:54:37 +0900

    프로젝트 기본 세팅 첫 커밋

---
+++ src/main/java/com/bohouse/ota/OtaSpplierConnectorApplication.java
@@ -0,0 +1,13 @@
+package com.bohouse.ota;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtaSpplierConnectorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OtaSpplierConnectorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bohouse/ota/OtaSpplierConnectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bohouse.ota;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtaSpplierConnectorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
