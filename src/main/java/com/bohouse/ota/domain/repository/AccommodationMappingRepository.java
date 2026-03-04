462c8f8f318c3819c7f147b3b2b8a25d47907b48
김보근 <intulda@naver.com>
2026-03-04 13:26:39 +0900

    fix: 사용하지 않는 인터페이스 제거

--- src/main/java/com/bohouse/ota/domain/repository/AccommodationMappingRepository.java
+++ src/main/java/com/bohouse/ota/domain/repository/AccommodationMappingRepository.java
@@ -5,12 +5,9 @@ import com.bohouse.ota.domain.model.AccommodationMapping;
 import com.bohouse.ota.domain.model.SupplierType;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-import java.util.function.Function;
 import java.util.function.Supplier;
 
 @Repository
 public interface AccommodationMappingRepository {
     FindOrCreateResult<AccommodationMapping> findOrCreate(SupplierType supplierType, String externalId,  Supplier<Accommodation> accommodation);
-    Optional<AccommodationMapping> findBy(String externalId, SupplierType supplierType);
 }
--- src/main/java/com/bohouse/ota/domain/repository/AccommodationRepository.java
+++ src/main/java/com/bohouse/ota/domain/repository/AccommodationRepository.java
@@ -4,11 +4,7 @@ import com.bohouse.ota.domain.model.Accommodation;
 import com.bohouse.ota.domain.model.SupplierType;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface AccommodationRepository {
-    Optional<Accommodation> findByExternal(String id, SupplierType supplierType);
-
     Accommodation save(String externalId, SupplierType supplierType, Accommodation accommodation);
 }
